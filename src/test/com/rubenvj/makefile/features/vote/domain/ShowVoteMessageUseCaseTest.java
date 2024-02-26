6be6cd062dcf7864208aa5dee54f153bc2f1e6c3
Rubén Vallejo <146753439+Rubenvalleho@users.noreply.github.com>
2024-02-26 18:00:57 +0100

    Estructura de los test

---
+++ src/test/com/rubenvj/makefile/features/vote/domain/ShowVoteMessageUseCaseTest.java
@@ -0,0 +1,29 @@
+package test.com.rubenvj.makefile.features.vote.domain;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ShowVoteMessageUseCaseTest {
+
+    @BeforeEach
+    public void saveUp() {
+
+    }
+
+    AfterEach
+    public void tearsDown() {
+
+    }
+
+    @Test
+    public void cuandoEsMenorQueCincoNoMuestraElMensaje() {
+        //Given
+
+
+        //When
+
+
+        //Then: Asserts
+    }
+}
